d028d9ec9376aa1c7c60626d5290653ff8b88d98
my-website <isaiahmokua003@gmail.com>
2025-06-17 15:34:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
-users = {"admin": "password123"}
+users = {"admin": "654321"}
 
 @app.route("/")
 def home():
