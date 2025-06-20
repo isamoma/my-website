0a85d6d574796b32a250ca8de6a8b7a9e1097d56
my-website <isaiahmokua003@gmail.com>
2025-06-20 13:17:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('admin_panel.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
