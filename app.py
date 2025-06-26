49ccbbb360318d91ac0415ce5a987cf4f22b4ec0
my-website <isaiahmokua003@gmail.com>
2025-06-26 13:25:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.secret_key = 'yoursecretkey'
 # Database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+users{}
 
 # Login manager setup (optional for user accounts)
 login_manager = LoginManager()
